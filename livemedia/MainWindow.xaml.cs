680299c858a185acb2faf1b79169ebef7b27bfc0
oguzdelioglu <kemerkaya@hotmail.com.tr>
2017-12-31 03:05:07 +0300

    LiveMedia TV and Radio

---
+++ livemedia/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace livemedia
+{
+	public partial class MainWindow : Window
+	{
+		private bool sbAyarlarAcikmi = false;
+        private Storyboard sbAc,sbKapa;
+        
+		public MainWindow()
+		{
+			this.InitializeComponent();
+            sbAc = (this.Resources["sbAyarlarAc"] as Storyboard);
+            sbKapa = (this.Resources["sbAyarlarKapat"] as Storyboard);
+		}
+       
+		private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+		{
+            MedyaListGuncelle();
+		}
+
+        private void btnMedyaEkle_Click(object sender, RoutedEventArgs e)
+        {
+            MedyaEkle();
+        }
+
+        private void menuAyarlar_Click(object sender, RoutedEventArgs e)
+        {
+            if (sbAyarlarAcikmi)
+            {
+                sbKapa.Begin();
+                sbAyarlarAcikmi = false;
+            }
+            else
+            {
+                sbAc.Begin();
+                sbAyarlarAcikmi = true;
+            }
+        }
+	}
+}
---
+++ livemedia/Process.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace livemedia
+{
+    public partial class MainWindow
+    {
+        XDocument ayarlar = XDocument.Load("settings.xml");
+        
+        public ListBoxItem lbItemDondur(string ad, string url)
+        {
+            ListBoxItem tmp = new ListBoxItem();
+            tmp.Content = ad;
+            tmp.MouseDoubleClick += (s, e) =>
+            {
+                mediaPlayer.Source = new Uri(url);
+            };
+
+            return tmp;
+        }
+        public void MedyaListGuncelle() 
+        {
+            lbTvList.Items.Clear();
+            lbRadyoList.Items.Clear();
+
+            var radyolar = from m in ayarlar.Descendants("Radios").Elements("radio")
+                           select new 
+                           {
+                                radyoAd = m.Element("ad").Value,
+                                radyoUrl = m.Element("url").Value
+                           };
+
+            var tvler = from m in ayarlar.Descendants("TVS").Elements("tv")
+                           select new
+                           {
+                               tvAd = m.Element("ad").Value,
+                               tvUrl = m.Element("url").Value
+                           };
+            foreach (var i in tvler)
+                lbTvList.Items.Add(lbItemDondur(i.tvAd,i.tvUrl));
+
+            foreach (var i in radyolar)
+                lbRadyoList.Items.Add(lbItemDondur(i.radyoAd, i.radyoUrl));
+
+        }
+        public void MedyaTurDenetle(string tur) 
+        {
+            ayarlar.Element("Medias").Element((tur == "radyo") ? "Radios" : "TVS").
+                Add(new XElement(tur, new XElement("ad", txtMedyaAdi.Text), new XElement("url", txtMedyaUrl.Text)));
+        }
+        public void MedyaEkle() 
+        {
+
+            MedyaTurDenetle((radioTv.IsChecked == true)?"tv":"radio");
+            ayarlar.Save("settings.xml");
+            MedyaListGuncelle();
+        
+        }
+    }
+}
